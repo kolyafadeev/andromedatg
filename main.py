fd655b66c5b5a599f720df5d4a920017ed316c8f
kolyafadeev <113797609+kolyafadeev@users.noreply.github.com>
2023-01-21 17:30:27 +0300

    мой первый бот в тг

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
-botle = """
-    КОМПОТ = ЯБЛОКИ И ВИШНЯ.
-    Не суди книгу по обложке
-    ааааа не знаю чо писать, напишу про китайскую мудрость.
-    етигомоп
-"""
+from telegram.ext import Updater, CommandHandler, CallbackContext
+from config import TOKEN
+from telegram import Update
+
+
+def start(update: Update, context: CallbackContext):#колбэк - функция
+    #update- обновление чата, в котором находится свежее собщение
+    #context - весь чат, мы будем использовать в качестве рюкзака
+    name = update.message.from_user.full_name
+    update.message.reply_text(f"Летсго бро {name}")
+    update.message.reply_text("""
+                                Я разумный интеллект, вот что я могу:
+                                /start - справка 
+                                /bye - прощание
+                                /zitata - великие слова великих людей
+                                """)
+
+def bye (update: Update, context: CallbackContext):
+    name = update.message.from_user.full_name
+    update.message.reply_text(f"НЕ УХАДИ, позялуйста... {name}")
+
+def zitata (update: Update, context: CallbackContext):
+    name = update.message.from_user.full_name
+    update.message.reply_text(f"Чтобы идти к цели, нужно встать с постели {name}")
+
+print("Сервер запущен")
+updater = Updater(TOKEN)
+dispatcher = updater.dispatcher
+
+
+start_handler = CommandHandler("start", start)
+bye_handler = CommandHandler ("bye", bye)
+zitata_handler = CommandHandler ("zitata", zitata)
+
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(bye_handler)
+dispatcher.add_handler(zitata_handler)
+
+updater.start_polling()
+updater.idle()
